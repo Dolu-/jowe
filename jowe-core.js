c9dbaf105f9cacf6dd1bf51e8698fe1d1bfd1408
ouebfrance@gmail.com <ouebfrance@gmail.com@e8d61e83-cc5c-d265-db1f-c65bcb687ded>
2011-02-02 14:41:15 +0000

    + Added Mash() and Alea() objects, will be used to generate identical maps from same seed + Added minSide and maxSide (limits for map generation) ~ Replaced width and height by side, as we work with square ~ Replaced avgColorFrom3() and avgColorFrom4() by addDelta() (which include the random call too) ~ Replaced randomPitch() by randomMinMax() ~ Optimized initialize(), generate(), smooth() and crop()

--- jowe-core.js
+++ jowe-core.js
@@ -31,37 +31,53 @@ Thanks to Radu Privantu for the HME project and to Stefan Hellkvist for the
 SDL plasma code.
 
 ********************************************************************************
+The Mash() and Alea() objects : Johannes Baagøe <baagoe@baagoe.com>, 2010
+From http://baagoe.com/en/RandomMusings/javascript/
+
+********************************************************************************
 
 Details about generation time in milliseconds - for 'doHeightMap()' (beware it's average time) :
 Execution time could be extremely different depending on your configuration and many other
 several factors, you would have to be really careful by interpreting the results below.
-___________________________________________________________________________________________
-|                         |                   |         |         |           |           |
-| Configuration           | Navigator         | 256x256 | 512x512 | 1024x1024 | 2048x2048 |
-|_________________________|___________________|_________|_________|___________|___________|
-| Intel T2400@1.83Ghz/2Gb | Google Chrome 8.0 |    50   |   200   |    700    |   2800    |
-|                         | Firefox 4.0b9     |   100   |   400   |   1500    |   3300(*) |
-|                         | Firefox 3.6 (**)  |   180   |   650   |   2400    |      -    |
-|                         |                   |         |         |           |           |
+_______________________________________________________________________________ _ _ _ _ _ _ _ _
+|                         |                   |         |         |           |               |
+| Configuration           | Navigator         | 256x256 | 512x512 | 1024x1024 | 2048x2048(*3) |
+|_________________________|___________________|_________|_________|___________|_ _ _ _ _ _ _ _|
+| Intel T2400@1.83Ghz/2Gb | Google Chrome 8.0 |    50   |   150   |    550    |     2100      |
+|                         | Firefox 4.0b9     |    60   |   220   |    800    |     3300(*1)  |
+|                         | Firefox 3.6 (*2)  |   180   |   650   |   2400    |       -       |
+|                         |                   |         |         |           |               |
 
-(*) After several calls, generation of large maps takes a lot of time (memory issue?).
+(*1) After several calls, generation of large maps takes a lot of time (memory issue?).
 It does not occur with smaller ones.
+(*2) It works and time is correct, but it needs to optimize the jowe-ui, as displaying grid
+takes time when there are a lot of cell (time is correct with big zoom).
+(*3) On 2011-02-01, I decided to lower the limit of the map size to avoid long generation time.
+Nonsense if it's not usable. Time for 2048x2048 map is left only for information purpose.
+
 Here is a bunch of time (in ms) that I obtain with several calls in Google Chrome 8.0 (for 2000x2000) :
+~previous release :
 initialize          =  172    147     95    103
 generate            = 1692   1711   1722   1703
 smooth              =  811    786    793    773
 crop                =  135    186    185    185
 doHeightMap (Total) = 2810   2830   2795   2764
-
-(**) It works and time is correct, but it needs to optimize the jowe-ui, as displaying grid
-takes time when there are a lot of cell (time is correct with big zoom).
+~current release :
+initialize          =  250    280    212    219
+generate            =  999   1507   1014   1003
+smooth              =  700    706    702    697
+crop                =   42     48     43     44
+doHeightMap (Total) = 1991   2541   1971   1963
+=> Good improvements in crop and generate.
+=> Small one in smooth
+=> Regression in initialize (?!)
+********************************************************************************
 
 NOTICE :
 - Basically, the only function to be called here is "doHeightMap".
   It could be placed or duplicated (and renamed) elsewhere (it will probably happen in a
   next release).
   Every other piece of code in here is only for the HeightMap object.
-
 */
 
 /*
@@ -73,12 +89,17 @@ Random Height Map Generator Object.
 // Create new map object (as global).
 var myMap = new HeightMap();
 
+// Random object/class.
+var rand = new Alea('');
+
 function HeightMap(arg_pitch, arg_ratio) {
 
-    // Size of the current map [0 .. _Height], [0 .. _Width].
-    // Always have to be N^2 x N^2 ("diamond square" algorithm. Better looking results with squares).
-    this.width = 128;
-    this.height = 128;
+    // Size of the current map [0 .. side], [0 .. side].
+    // Always have to be (N^2)+1 x (N^2)+1 ("diamond square" algorithm. Better looking results with squares).
+    // We only need to store it once (side = width = height).
+    var side = 129;
+    // Set minimum and maximum size for a side
+    var minSide = 4, maxSide = 2040;
     
     // Maximum elevation for current map [0 .. _Pitch].
     // You will have to adjust the color managment in "jowe-ui" to fit your elevation.
@@ -98,64 +119,58 @@ function HeightMap(arg_pitch, arg_ratio) {
     // Array with the world map.
     this.item = [];
     
-    var that = this;
-
-    var mr = Math.floor;
     /*
      * [Privileged method] initialize()
      *
      * Initialize the array.
-     * h is the default height.
+     * h = default height.
      */
     this.initialize = function(h) {
-        var x, a = [], b = [], tw = this.width, th = this.height;
-        if ((h === undefined) || (h === null)) {
-          h = -1;
-        }
-
-        b.length = th;
-        for (x = 0; x < th; x++) b[x] = h;
-        a.length = tw;
-        for (x = 0; x < tw; x++) a[x] = b.slice();
-
-        this.item = a;
+        var x = side, a = this.item, b = [];
+        a.length = b.length = side;
+        while (x) b[--x] = h;
+        x = side;
+        while (x) a[--x] = b.slice();
     }
-    
+        
     /*
-     * [Private method] randomPitch
+     * [Private method] randomMinMax
      *
-     * Return random height between min and max (included).
+     * Return random height between min and max (included),
+     * result is rounded (~~ => Math.floor).
      */
-    function randomPitch(min, max) {
-        return mr((Math.random() * ((max - min) + 1)) + min);
+    function randomMinMax(min, max) {
+        return ~~((Math.random() * ((max - min) + 1)) + min);
     }
 
     /*
-     * [Private method] avgColorFrom4
+     * [Privileged method] fillCorners
      *
-     * Return the average height of the 4 corners of a square.
+     * Set random height for each corner (initialize the map).
+     * (called once at initialization)
      */
-    function avgColorFrom4(x1, y1, x2, y2, avg_pitch) {
-        var avg = (that.item[x1][y1] + that.item[x1][y2] + that.item[x2][y2] + that.item[x2][y1]) / 4;
-        if (0 < avg_pitch) avg += randomPitch(-avg_pitch, avg_pitch);
-        avg = mr(avg);
-        if (pitch < avg) avg = pitch;
-        if (0 > avg) avg = 0;
-        return avg;
+    this.fillCorners = function(overwrite) {
+        if (overwrite || (0 > this.item[0][0]))
+            this.item[0][0] = randomMinMax(0, pitch);
+        if (overwrite || (0 > this.item[side - 1][0]))
+            this.item[side - 1][0] = randomMinMax(0, pitch);
+        if (overwrite || (0 > this.item[side - 1][side - 1]))
+            this.item[side - 1][side - 1] = randomMinMax(0,pitch);
+        if (overwrite || (0 > this.item[0][side - 1]))
+            this.item[0][side - 1] = randomMinMax(0, pitch);
     }
 
     /*
-     * [Private method] avgColorFrom3
+     * [Private method] addDelta
      *
-     * Return the average height of 3 points.
+     * Return a value added with a random delta.
+     * If the delta is more than 0, it adds to "avg" a random value between "-delta" and "+delta".
+     * In fact it sets the height between two points. The closer there are, the lower the delta will be.
+     * The function ensure that the result is positive and less than the pitch (max height).
      */
-    function avgColorFrom3(x1, y1, x2, y2, x3, y3, avg_pitch) {
-        var avg = (that.item[x1][y1] + that.item[x2][y2] + that.item[x3][y3]) / 3;
-        if (0 < avg_pitch) avg += randomPitch(-avg_pitch, avg_pitch);
-        avg = mr(avg);
-        if (pitch < avg) avg = pitch;
-        if (0 > avg) avg = 0;
-        return avg;
+    function addDelta(avg, delta) {
+        if (0 < delta) avg += (rand() * ((delta << 1) + 1)) - delta;
+        return (pitch < avg) ? pitch : (0 > avg) ? 0 : ~~avg ;
     }
 
     
@@ -165,72 +180,53 @@ function HeightMap(arg_pitch, arg_ratio) {
      * Generate a random map.
      * Parameters indicates "top/left" and "right/bottom" limits.
      */
-    this.generate = function(x1, y1, x2, y2, x_mid, y_mid) {
-
-        var my_pitch = mr((x2 - x_mid) / ratio);
+    this.generate = function(x1, y1, x2, y2, xm, ym) {
+        
+        var delta = ~~((x2 - xm) / ratio), a = this.item;
 
         // Set a random height for the middle of the current square.
-        if (0 > this.item[x_mid][y_mid]) this.item[x_mid][y_mid] = avgColorFrom4(x1, y1, x2, y2, my_pitch);
+        // addDelta is called with the average height of the 4 points.
+        if (0 > a[xm][ym]) a[xm][ym] = addDelta((a[x1][y1] + a[x1][y2] + a[x2][y2] + a[x2][y1]) >> 2, delta);
       
         // Set a random height for the middle of the hypotenuse of each triangle.
-        if (0 > this.item[x_mid][y1]) this.item[x_mid][y1] = avgColorFrom3(x1, y1, x2, y1, x_mid, y_mid, my_pitch);
-        if (0 > this.item[x_mid][y2]) this.item[x_mid][y2] = avgColorFrom3(x1, y2, x2, y2, x_mid, y_mid, my_pitch);
-        if (0 > this.item[x2][y_mid]) this.item[x2][y_mid] = avgColorFrom3(x2, y1, x2, y2, x_mid, y_mid, my_pitch);
-        if (0 > this.item[x1][y_mid]) this.item[x1][y_mid] = avgColorFrom3(x1, y1, x1, y2, x_mid, y_mid, my_pitch);
+        // addDelta is called with the average height of the 3 points.
+        if (0 > a[xm][y1]) a[xm][y1] = addDelta((a[x1][y1] + a[x2][y1] + a[xm][ym]) / 3, delta);
+        if (0 > a[xm][y2]) a[xm][y2] = addDelta((a[x1][y2] + a[x2][y2] + a[xm][ym]) / 3, delta);
+        if (0 > a[x2][ym]) a[x2][ym] = addDelta((a[x2][y1] + a[x2][y2] + a[xm][ym]) / 3, delta);
+        if (0 > a[x1][ym]) a[x1][ym] = addDelta((a[x1][y1] + a[x1][y2] + a[xm][ym]) / 3, delta);
 
-        if (((x2 - x1) > 2) || ((y2 - y1) > 2))
-        {
-            var x_calc = mr((x_mid - x1) / 2);
-            var x_mid1 = x_mid - x_calc, x_mid2 = x_mid + x_calc;
-            var y_mid1 = y_mid - x_calc, y_mid2 = y_mid + x_calc;
-
-            this.generate(x_mid, y_mid, x2, y2, x_mid2, y_mid2);
-            this.generate(x1, y_mid, x_mid, y2, x_mid1, y_mid2);
-            this.generate(x1, y1, x_mid, y_mid, x_mid1, y_mid1);
-            this.generate(x_mid, y1, x2, y_mid, x_mid2, y_mid1);
+        if (((x2 - x1) > 2) || ((y2 - y1) > 2)) {
+            delta = (xm - x1) >> 1;
+            var xm1 = xm - delta, xm2 = xm + delta;
+            var ymi = ym + delta;
+            this.generate(xm, ym, x2, y2, xm2, ymi);
+            this.generate(x1, ym, xm, y2, xm1, ymi);
+            ymi = ym - delta;
+            this.generate(x1, y1, xm, ym, xm1, ymi);
+            this.generate(xm, y1, x2, ym, xm2, ymi);
         }
     }
-    
-    /*
-     * [Privileged method] fillCorners
-     *
-     * Set random height for each corner (initialize the map).
-     * (called once at initialization)
-     */
-    this.fillCorners = function(overwrite) {
-        if (overwrite || (0 > this.item[0][0]))
-            this.item[0][0] = randomPitch(0, pitch);
-        if (overwrite || (0 > this.item[this.width - 1][0]))
-            this.item[this.width - 1][0] = randomPitch(0, pitch);
-        if (overwrite || (0 > this.item[this.width - 1][this.height - 1]))
-            this.item[this.width - 1][this.height - 1] = randomPitch(0,pitch);
-        if (overwrite || (0 > this.item[0][this.height - 1]))
-            this.item[0][this.height - 1] = randomPitch(0, pitch);
-    }
 
     /*
-     * [Privileged method] smoothMap
+     * [Privileged method] smooth
      *
      * Set cells height to be closer to other adjacent cells.
      */
     this.smooth = function() {
-        var x, y, sum, w = this.width - 1, h = this.height - 1, xm1 = [], xp1 = [];
-        for (x = 1; x < h ; x++) {
-            xm1 = this.item[x-1];
-            sum = this.item[x][1];
-            xp1 = this.item[x+1];
-            for (y = 1; y < w ; y++) {
+        var x, y, sum, a = this.item, s = side - 1, xm1 = [], xp1 = [];
+        for (x = 1; x < s ; x++) {
+            xm1 = a[x-1];
+            sum = a[x][1];
+            xp1 = a[x+1];
+            for (y = 1; y < s ; y++) {
 
-                sum +=  xm1[y-1]  + xm1[y  ]  + xm1[y+1]
-                      + this.item[x  ][y  ]
-                      + this.item[x  ][y+1]
-                      + xp1[y-1]  + xp1[y  ]  + xp1[y+1];
+                sum +=  xm1[y-1] + xm1[y] + xm1[y+1] + a[x][y] + a[x][y+1] + xp1[y-1] + xp1[y] + xp1[y+1];
                 
                 sum = 4 < (sum % 9) ? (sum / 9) + 1 : sum / 9;
-                sum = mr(sum);
+                sum = ~~sum;
                 
                 if (pitch < sum) sum = pitch;
-                this.item[x][y] = sum;
+                a[x][y] = sum;
             }
         }
     }
@@ -241,16 +237,10 @@ function HeightMap(arg_pitch, arg_ratio) {
      * Crop current map according to specific size.
      */
     this.crop = function(width, height, cropsize) {
-        var x, xmax = width + cropsize, y, ymax = height + cropsize, a = [];
-        // Crop the working map to get the requested map.
-        for(x = cropsize; x < xmax; x++) {
-            a = this.item[x - cropsize];
-            for(y = cropsize; y < ymax; y++) {
-                a[y - cropsize] = this.item[x][y];
-            }
-            a.length = height;
-        }
-        this.item.length = width;
+        var a =this.item.slice(cropsize, width + cropsize);
+        var x = a.length;
+        while (x--) a[x] = a[x].slice(cropsize, height + cropsize);
+        this.item = a;
     }
 
 
@@ -264,18 +254,18 @@ function HeightMap(arg_pitch, arg_ratio) {
      */
      this.doMap = function(width, height) {
         // Default values if none provided.
-        // It also limits size to 2048x2048, to avoid big generation time.
+        // It also limits size to [maxSide]x[maxSide], to avoid big generation time.
         if ((width == null) || (height == null) ||
-            (width < 2) || (height < 2)||
-            (width > 2044) || (height > 2044)) {
+            (width < minSide) || (height < minSide)||
+            (width > maxSide) || (height > maxSide)) {
 
             width = 128;
             height = 128;
         }
 
         // We have better results with square and N^2 x N^2 maps,
-        // Let's keep the bigger side
-        var side = Math.max(width, height);
+        // Let's keep the bigger side.
+        side = Math.max(width, height);
 
         // Look for 2^n size (better results from 2^7).
         var n = 7;
@@ -284,28 +274,25 @@ function HeightMap(arg_pitch, arg_ratio) {
 
         // Create new map object.
         // At this stage, working size will be ((Math.pow(2, n) + 1) x (Math.pow(2, n) + 1))
-        this.width = Math.pow(2, n) + 1;
-        this.height = Math.pow(2, n) + 1;
+        side = Math.pow(2, n) + 1;
         
         // For debug purpose.
-        //dbg_date[2] = new Date();
+        // dbg_date[2] = new Date();
         
         // Initialize height.
-        this.initialize();
+        this.initialize(-1);
 
         // Initialize corners.
         this.fillCorners(true);
 
         // For debug purpose.
-        //dbg_date[3] = new Date();
+        // dbg_date[3] = new Date();
 
         // Do map!
-        this.generate(0, 0, this.width - 1, this.height - 1,
-                        mr((this.width - 1) / 2),
-                        mr((this.height - 1) / 2));
+        this.generate(0, 0, side - 1, side - 1,(side - 1) >> 1,(side - 1) >> 1);
 
         // For debug purpose.
-        //dbg_date[4] = new Date();
+        // dbg_date[4] = new Date();
 
         // Smooth map to remove weird points.
         this.smooth();
@@ -322,18 +309,74 @@ function doHeightMap(width, height)
     var Crop = 1;
 
     // For debug purpose.
-    //dbg_date[1] = new Date();
+    // dbg_date[1] = new Date();
     
     // We'll exclude all the border lines to avoid weird point,
     // so we enlarge the map size with (width+2) and (height+2).
     myMap.doMap(width + (2 * Crop), height + (2 * Crop));
 
     // For debug purpose.
-    //dbg_date[5] = new Date();
+    // dbg_date[5] = new Date();
 
     // Crop the working map to get the requested map.
     myMap.crop(width, height, Crop);
 
     // For debug purpose.
-    //dbg_date[6] = new Date();
+    // dbg_date[6] = new Date();
+}
+
+// From http://baagoe.com/en/RandomMusings/javascript/
+// Johannes Baagøe <baagoe@baagoe.com>, 2010
+function Mash() {
+  var n = 0xefc8249d;
+
+  var mash = function(data) {
+    data = data.toString();
+    for (var i = 0; i < data.length; i++) {
+      n += data.charCodeAt(i);
+      var h = 0.02519603282416938 * n;
+      n = h >>> 0;
+      h -= n;
+      h *= n;
+      n = h >>> 0;
+      h -= n;
+      n += h * 0x100000000; // 2^32
+    }
+    return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
+  };
+  return mash;
+}
+
+// From http://baagoe.com/en/RandomMusings/javascript/
+// Johannes Baagøe <baagoe@baagoe.com>, 2010
+function Alea() {
+  return (function(args) {
+    var s0 = 0, s1 = 0, s2 = 0, c = 1, mash = Mash(), p1 = 2091639, p2 = 2.3283064365386963e-10; // 2^-32
+
+    if (args.length == 0) {
+      args = [+new Date];
+    }
+    s0 = mash(' ');
+    s1 = mash(' ');
+    s2 = mash(' ');
+
+    for (var i = 0; i < args.length; i++) {
+      s0 -= mash(args[i]);
+      if (s0 < 0) s0 += 1;
+      s1 -= mash(args[i]);
+      if (s1 < 0) s1 += 1;
+      s2 -= mash(args[i]);
+      if (s2 < 0) s2 += 1;
+    }
+    mash = null;
+
+    var random = function() {
+      var t = p1 * s0 + c * p2;
+      s0 = s1 = s2;
+      return s2 = t - (c = t | 0);
+    };
+    random.args = args;
+    return random;
+
+  } (Array.prototype.slice.call(arguments)));
 }
